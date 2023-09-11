0617d2246a738f6771dcaffa45780ba0840d0f0d
Gilbert Ardila <gilbertferney@gmail.com>
2023-09-11 06:02:11 -0500

    creando database file

---
+++ database/db.sql
@@ -0,0 +1,16 @@
+CREATE TABLE categories (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  description TEXT NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE products (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  description TEXT NOT NULL,
+  price DECIMAL(10,2) NOT NULL,
+  image VARCHAR(255) NOT NULL,
+  category_id INT NOT NULL,
+  PRIMARY KEY (id)
+);
