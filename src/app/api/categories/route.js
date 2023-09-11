e8dbb5634a25b5c503f1530f7dbc9eff77f4767a
Gilbert Ardila <gilbertferney@gmail.com>
2023-09-11 06:30:17 -0500

    creando productos y categorias

---
+++ src/app/api/categories/route.js
@@ -0,0 +1,26 @@
+import { NextResponse } from "next/server";
+import { conn } from "@/libs/mysql";
+
+export async function GET(){
+    const response = await conn.query('SELECT * FROM categories');
+    console.log(response)
+     return NextResponse.json(response);
+    }
+
+export async function POST(request){
+    try {
+        const {name,description} = await request.json();
+    const newCategory = await conn.query('INSERT INTO categories SET ?', {name,description});
+    console.log(newCategory);
+    return NextResponse.json({
+        status: 200,
+        id:newCategory.insertId,
+        name,
+        description
+    });
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json({status: 500, message: error.sqlMessage});
+        
+    }
+    }
--- src/app/api/products/route.js
+++ src/app/api/products/route.js
@@ -2,9 +2,29 @@
  import { conn } from "@/libs/mysql";
 
  export async function GET(){
-   const response = await conn.query('SELECT NOW()');
+   const response = await conn.query('SELECT * FROM products');
    console.log(response)
-    return NextResponse.json({message: 'hello from get'});
+    return NextResponse.json(response);
  }
 
  
+export async function POST(request){
+  try {
+    const {name,description,price,image,category_id} = await request.json();
+  const newProduct = await conn.query('INSERT INTO products SET ?', {name,description,price,image,category_id});
+  console.log(newProduct);
+  return NextResponse.json({
+    status: 200,
+    id:newProduct.insertId,
+    name,
+    description,
+    price,
+    image,
+    category_id
+  });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({status: 500, message: error.sqlMessage});
+    
+  }
+}
